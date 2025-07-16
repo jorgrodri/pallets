f2c56f5865f64c20bfa2a33b794ddade5d4d9798
jorgrodri <156688014+jorgrodri@users.noreply.github.com>
2025-07-15 23:51:29 -0400

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('pallet');
+  protected readonly title = signal('Mi Agente Digital');
 }
