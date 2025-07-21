bd83977c22d94cbda98be8662d20f334b5c0255b
jorgrodri <156688014+jorgrodri@users.noreply.github.com>
2025-07-21 13:25:18 -0400

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Jorge Rodriguez');
+  protected readonly title = signal(' ing Jorge Rodriguez ');
 }
